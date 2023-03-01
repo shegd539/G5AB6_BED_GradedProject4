65ccea1151625581ebe7fc7ec7d163a926a416d2
shanhegd <shanhegd@gmail.com>
2023-03-01 11:38:17 +0530

    added springboot security
    added springboot security

--- src/main/java/com/greatlearning/empApi/G5Ab6BedGradedProject4Application.java
+++ src/main/java/com/greatlearning/empApi/G5Ab6BedGradedProject4Application.java
@@ -2,8 +2,10 @@ package com.greatlearning.empApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan( basePackages = "org.springframework.security.core.userdetails" )
 public class G5Ab6BedGradedProject4Application {
 
 	public static void main(String[] args) {
